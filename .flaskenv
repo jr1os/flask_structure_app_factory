e1a43075acdd38e3b8aab780184005b6ddaa3a3a
Ali Rios <ali.rios@gmail.com>
2022-05-04 16:26:43 -0300

    :recycle: sqlalchemy database

---
+++ .flaskenv
@@ -0,0 +1,3 @@
+export FLASK_APP=core
+export FLASK_ENV=development
+
---
+++ core/models/video.py
@@ -0,0 +1,7 @@
+from ..extensions import db
+
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    url = db.Column(db.String(50))
